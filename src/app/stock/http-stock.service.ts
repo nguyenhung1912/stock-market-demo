6d02432c03e39277e238502268c77f65f8faa5f5
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-03-16 23:49:48 +0700

    feat: add HttpStockService for CRUD operations with stock API

--- src/app/stock/http-stock.service.ts
+++ src/app/stock/http-stock.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Stock } from '../model/stock';
 
 export interface StockApi {
     id?: number;
@@ -26,7 +27,27 @@ export class HttpStockService {
     }
 
     // GET By ID
-    getStockById(id: number): Observable<StockApi[]> {
-        return this.http.get<StockApi[]>(`${this.apiUrl}/${id}`);
+    getStockById(id: number): Observable<StockApi> {
+        return this.http.get<StockApi>(`${this.apiUrl}/${id}`);
+    }
+
+    // POST
+    createStock(stock: Stock): Observable<StockApi> {
+        return this.http.post<StockApi>(this.apiUrl, stock);
+    }
+
+    // PUT
+    updateStock(id: number, stock: Stock): Observable<StockApi>{
+        return this.http.put<StockApi>(`${this.apiUrl}/${id}`, stock);
+    }
+
+    // PATCH
+    patchStock(id: number, partial: Partial<StockApi>): Observable<StockApi>{
+        return this.http.patch<StockApi>(`${this.apiUrl}/${id}`, partial);
+    }
+
+    // DELETE
+    deleteStock(id: number): Observable<void>{
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
     }
 }
