a7a55b4dd08e3112437375d3bfb5fcbb4dde89f8
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-04-06 22:46:59 +0700

    feat: update routing configuration to include auth guard for protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './features/auth/components/login/login.component';
-import { CreateStockReactiveComponent } from './stock/create-stock-reactive/create-stock-reactive.component';
 import { RegisterComponent } from './features/auth/components/register/register.component';
-import { StockDetailsComponent } from './stock/stock-details/stock-details.component';
 import { StockListComponent } from './shared/stock-list/stock-list.component';
+import { CreateStockReactiveComponent } from './stock/create-stock-reactive/create-stock-reactive.component';
+import { StockDetailsComponent } from './stock/stock-details/stock-details.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full', },
-  { path: 'login', component: LoginComponent, },
-  { path: 'register', component: RegisterComponent, },
-  { path: 'stocks', component: StockListComponent},
-  { path: 'favorites', component: StockListComponent },
-  { path: 'stocks/create', component: CreateStockReactiveComponent},
-  { path: 'stock/:id', component: StockDetailsComponent},
-  { path: '**', redirectTo: '/login' }
-];  
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: 'stocks', component: StockListComponent, canActivate: [authGuard] },
+  { path: 'favorites', component: StockListComponent, canActivate: [authGuard] },
+  { path: 'create', component: CreateStockReactiveComponent, canActivate: [authGuard] },
+  { path: 'stock/:id', component: StockDetailsComponent, canActivate: [authGuard] },
+  
+  { path: '**', redirectTo: 'stocks' }
+];
