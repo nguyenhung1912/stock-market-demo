9c2a5b048f573b3f25aa258ff2291266c258f4eb
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-04-11 19:43:50 +0700

    feat: implement reactive form for login with validation and error messages

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../../../core/services/auth.service';
@@ -8,31 +8,46 @@ import { AuthService } from '../../../../core/services/auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  username = '';
-  password = '';
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private authService: AuthService,
     private router: Router,
     private toastr: ToastrService,
   ) {}
 
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
   onLogin() {
-    this.authService.login(this.username, this.password).subscribe({
-      next: (users) => {
-        if (users.length > 0) {
-          this.toastr.success('Login successful!');
-          this.router.navigate(['/stocks']);
-        } else {
-          this.toastr.error('Invalid username or password!');
-        }
-      },
-      error: () => this.toastr.error('Connection error! Check JSON Server.'),
-    });
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      const cleanUsername = username.trim();
+      const cleanPassword = password.trim();
+
+      this.authService.login(cleanUsername, cleanPassword).subscribe({
+        next: (users) => {
+          if (users.length > 0) {
+            this.toastr.success('Login successful!');
+            this.router.navigate(['/stocks']);
+          } else {
+            this.toastr.error('Invalid username or password!');
+          }
+        },
+        error: () => this.toastr.error('Connection error! Check JSON Server.'),
+      });
+    } else {
+      this.loginForm.markAllAsTouched(); 
+    }
   }
 }
